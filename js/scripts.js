779ccae4a2f5346b3ff58fe92b7e2131aa4b8d95
VenkaSri <venkasritharan@gmail.com>
2022-05-07 17:25:44 -0400

    dynamically added form elements, added ability to add card via the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 const addButton = document.querySelector('button');
-const bookName = document.querySelector('#bookName').value;
-const bookAuthor = document.querySelector('#bookAuthor').value;
-const bookPages = document.querySelector('#bookPages').value;
-const bTable = document.querySelector('table');
+// const bookName = document.querySelector('#bookName').value;
+// const bookAuthor = document.querySelector('#bookAuthor').value;
+// const bookPages = document.querySelector('#bookPages').value;
+// const bTable = document.querySelector('table');
+const mainCont = document.querySelector('.main-container');
 
 
 
 addButton.addEventListener('click', () => {
-    console.log('works');
-    addBookToLibrary();
-})
+  createInput();
+  let card = document.querySelector('.card');
+
+})  
+
+
+
+
+function sayHi() {
+  return 'hi';
+}
+
+
 
 let myLibrary = [];
 
@@ -19,16 +30,67 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary() {
-  myLibrary.push(new Book(bookName, bookAuthor, bookPages));
-    let tRow = document.createElement('tr');
-    let tCell = document.createElement('td');
-    bTable.append(tRow);
-    tRow.append(tCell);
-    let n = document.querySelector('td');
-  for(let x of myLibrary) {
-    n.innerHTML = x.title;
-    console.log(x.title);
+// function addBookToLibrary() {
+//   myLibrary.push(new Book(bookName, bookAuthor, bookPages));
+//     let tRow = document.createElement('tr');
+//     let tCell = document.createElement('td');
+//     bTable.append(tRow);
+//     tRow.append(tCell);
+//     let n = document.querySelector('td');
+//   for(let x of myLibrary) {
+//     n.innerHTML = x.title;
+//     console.log(x.title);
+// }
+// }
+
+
+
+let cardDiv = `
+    
+<div class="card">
+    <div class="book-info">
+        <input type="text" id="bookName" placeholder="Book name">
+        <input type="author" id="bookAuthor" placeholder="Author">
+        <input type="number" id="bookPages" placeholder="Pages">
+        <div class="book-read">
+            <input type="checkbox" id="readCheck" name="readCheck" value="Read">
+            <label for="readCheck">Read</label><br>
+        </div>
+        <div class="addBtn">Add</div>
+    </div>    
+</div>
+
+
+
+`;
+
+//form to add book
+function createInput() {
+
+  let div = document.createElement('div');
+  div.classList.add('card');
+  let divTwo = document.createElement('div');
+  divTwo.classList.add('book-info');
+  let book = document.createElement("input");
+  book.setAttribute("type", "text");
+  book.setAttribute("id", "bookName");
+  book.setAttribute("placeholder", "Book name");
+  let author = document.createElement("input");
+  author.setAttribute("type", "text");
+  author.setAttribute("id", "bookAuthor");
+  author.setAttribute("placeholder", "Author");
+  let page = document.createElement("input");
+  page.setAttribute("type", "text");
+  page.setAttribute("id", "bookPages");
+  page.setAttribute("placeholder", "Pages");
+
+  div.append(divTwo);
+  divTwo.append(book);
+  divTwo.append(author);
+  divTwo.append(page);
+
+  mainCont.append(div);
 }
-}
+
+
 
