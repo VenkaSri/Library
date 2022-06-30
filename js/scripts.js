e9866a024e92177bdad42c59dff156bf3a3df179
VenkaSri <venkasritharan@gmail.com>
2022-06-29 23:42:51 -0400

    added delete link once user changes book status, also added simple form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
-const addButton = document.querySelector('button');
+const addButton = document.querySelector("button");
 
 // const bTable = document.querySelector('table');
-const mainCont = document.querySelector('.main-container');
+const mainCont = document.querySelector(".main-container");
 let checkRead;
+let formSuccess;
 
-
-
-
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   createInput();
-
-
-})  
+});
 
 let myLibrary = [];
 
@@ -21,25 +17,45 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-
-
 function addBookToLibrary() {
-  myLibrary.push(new Book(bookName.value, bookAuthor.value, bookPages.value));
+  if(bookName.value === '' || bookAuthor.value === '' || bookPages.value === '') {
+    setTimeout(function() {
+      const myNode = document.querySelector(".card");
+    myNode.classList.add('shake');
+      setTimeout(function() {
+        const myNode = document.querySelector(".card");
+        myNode.classList.remove('shake');
+      }, 1000)
+    }, 0);
+    formSuccess = false;
+  } else {
+    myLibrary.push(new Book(bookName.value, bookAuthor.value, bookPages.value));
+    formSuccess = true;
+  }
+}
 
+function hidePopup() {
+  const myNode = document.querySelector(".card");
+  myNode.classList.add('shake');
+}
+
+function deleteClass() {
+  const myNode = document.querySelector(".card");
+  myNode.classList.remove('shake');
 }
 
 
-//form card 
+//form card
 function createInput() {
-  let addDiv = document.createElement('div');
-  addDiv.classList.add('addBtn');
-  addDiv.innerHTML = 'Add';
-  let mainDiv = document.createElement('div');
-  mainDiv.classList.add('card');
-  let infoDiv = document.createElement('div');
-  infoDiv.classList.add('book-info');
-  let readDiv = document.createElement('div');
-  readDiv.classList.add('book-read');
+  let addDiv = document.createElement("div");
+  addDiv.classList.add("addBtn");
+  addDiv.innerHTML = "Add";
+  let mainDiv = document.createElement("div");
+  mainDiv.classList.add("card");
+  let infoDiv = document.createElement("div");
+  infoDiv.classList.add("book-info");
+  let readDiv = document.createElement("div");
+  readDiv.classList.add("book-read");
   let book = document.createElement("input");
   book.setAttribute("type", "text");
   book.setAttribute("id", "bookName");
@@ -62,7 +78,7 @@ function createInput() {
   checkbox.setAttribute("value", "Read");
   let lbl = document.createElement("label");
   lbl.setAttribute("for", "readCheck");
-  lbl.innerHTML = 'Read';
+  lbl.innerHTML = "Read";
 
   mainDiv.append(infoDiv);
   infoDiv.append(book);
@@ -75,24 +91,26 @@ function createInput() {
 
   mainCont.insertBefore(mainDiv, mainCont.firstChild);
 
-  const addBtn = document.querySelector('.addBtn');
-  addBtn.addEventListener('click', () => {
+  const addBtn = document.querySelector(".addBtn");
+  addBtn.addEventListener("click", () => {
     addBookToLibrary();
-    const myNode = document.querySelector(".card");
-    myNode.innerHTML = '';
-    addValue();
-  
-  }) 
+    if(formSuccess) {
+      const myNode = document.querySelector(".card");
+      myNode.innerHTML = "";
+      addValue();
+    } 
+    
+  });
 
   Book.prototype = {
     read() {
-      if(checkbox.checked === true) {
+      if (checkbox.checked === true) {
         checkRead = true;
       } else {
         checkRead = false;
       }
-    }
-  }
+    },
+  };
 }
 
 function addValue() {
@@ -100,12 +118,18 @@ function addValue() {
   let author = myLibrary[0].author;
   let page = myLibrary[0].pages;
   const myNode = document.querySelector(".card");
-  let content = document.createElement('div');
+  let content = document.createElement("div");
+  let del = document.createElement("div");
   content.classList.add("created-card");
+  del.classList.add("del");
   myNode.append(content);
-  let nDiv = document.createElement('div');
-  let aDiv = document.createElement('div');
-  let pDiv = document.createElement('div');
+  let nDiv = document.createElement("div");
+  let aDiv = document.createElement("div");
+  let pDiv = document.createElement("div");
+  let delLink = document.createElement("a");
+  delLink.innerHTML = "Del";
+  content.append(del);
+  del.append(delLink);
   content.append(nDiv);
   content.append(aDiv);
   content.append(pDiv);
@@ -116,40 +140,43 @@ function addValue() {
 
   let lbl = document.createElement("label");
   content.append(lbl);
-lbl.classList.add("switch");
-let chk = document.createElement("input");
-chk.setAttribute("type", "checkbox");
-chk.id = "chkBox";
-let span = document.createElement("span");
-span.classList.add("slider");
-span.classList.add("round"); 
-lbl.append(chk);
-lbl.append(span);
+  lbl.classList.add("switch");
+  let chk = document.createElement("input");
+  chk.setAttribute("type", "checkbox");
+  chk.id = "chkBox";
+  let span = document.createElement("span");
+  span.classList.add("slider");
+  span.classList.add("round");
+  lbl.append(chk);
+  lbl.append(span);
 
-myLibrary[0].read();
-if(checkRead) {
-  chk.checked = true;
-} else {
-  chk.checked = false;
-}
-
-document.addEventListener('click',function(e){
-  if(e.target && e.target.id== 'chkBox'){
-    if(chk.checked === true) {
-      checkRead = true;
-    } else {
-      checkRead = false;
+  myLibrary[0].read();
+  if (checkRead) {
+    chk.checked = true;
+    toggleDel(del);
+  } else {
+    chk.checked = false;
+    toggleDel(del);
+  }
+  document.addEventListener("click", function (e) {
+    if (e.target && e.target.id == "chkBox") {
+      if (chk.checked === true) {
+        checkRead = true;
+        toggleDel(del);
+      } else {
+        checkRead = false;
+        toggleDel(del);
+      }
     }
-   }
-});
-  
+  });
+
   myLibrary.length = 0;
 }
 
-
-
-
-
-
-
-
+function toggleDel(div) {
+  if (checkRead == true) {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
